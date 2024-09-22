617d9ee9957238e0f9b57aabe38753f0a659cc54
Eduard Castro <eduard.cas07@gmail.com>
2024-09-22 15:10:02 -0500

    Add CI workflow for AWS SAM Book API

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: "module"
+    env: {
+        node: true,
+        es2021: true,
+        jest: true,
     },
     extends: [
-        "plugin:@typescript-eslint/recommended", // recommended rules from the @typescript-eslint/eslint-plugin
-        "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'eslint:recommended',
+        'plugin:node/recommended',
     ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    }
+        'no-unused-vars': 'warn',
+        'no-console': 'off',
+        'node/no-unsupported-features/es-syntax': [
+            'error',
+            { ignores: ['modules'] },
+        ],
+    },
 };
